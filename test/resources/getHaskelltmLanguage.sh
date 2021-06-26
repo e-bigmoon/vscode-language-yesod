e95cda8b4a7d5875ff0a2fc96103e378ac811aae
isao-takejib <isao.takeji.bigmoon@gmail.com>
2021-06-26 07:17:04 +0000

    Added the part to get the css syntax file.

--- test/resources/getHaskelltmLanguage.sh
+++ test/resources/getHaskelltmLanguage.sh
@@ -5,13 +5,30 @@ set -eux
 # shellcheck disable=SC2086
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 
-TAG=v3.4.0
-REPO="https://raw.githubusercontent.com/JustusAdam/language-haskell"
+HS_REPO="https://raw.githubusercontent.com/JustusAdam/language-haskell"
+HS_TAG=v3.4.0
 HS_SYNTAX_DIR="syntaxes"
-HS_LANGUAGE_FILE="haskell.YAML-tmLanguage"
 HS_LANGUAGE_JSON="haskell.tmLanguage.json"
+HS_LANGUAGE_FILE="haskell.YAML-tmLanguage"
 
-if [ ! -f "${SCRIPT_DIR}/${HS_LANGUAGE_FILE}" ]; then
-    wget -P "${SCRIPT_DIR}" "${REPO}/${TAG}/${HS_SYNTAX_DIR}/${HS_LANGUAGE_FILE}"
-    yq eval . "${SCRIPT_DIR}/${HS_LANGUAGE_FILE}" -j > "${SCRIPT_DIR}/${HS_LANGUAGE_JSON}"
-fi
+CSS_REPO="https://raw.githubusercontent.com/microsoft/vscode"
+CSS_TAG=1.57.1
+CSS_SYNTAX_DIR="extensions/css/syntaxes"
+CSS_LANGUAGE_JSON="css.tmLanguage.json"
+
+function getSyntaxFile() {
+    local syntaxDirUrl="$1"
+    local languageJson="$2"
+    local targetFile="${3:-${languageJson}}"
+
+    if [ ! -f "${SCRIPT_DIR}/${targetFile}" ]; then
+        wget -P "${SCRIPT_DIR}" "${syntaxDirUrl}/${targetFile}"
+
+        if [ ! "${targetFile}" = "${languageJson}" ]; then
+            yq eval . "${SCRIPT_DIR}/${targetFile}" -j > "${SCRIPT_DIR}/${languageJson}"
+        fi
+    fi  
+}
+
+getSyntaxFile "${HS_REPO}/${HS_TAG}/${HS_SYNTAX_DIR}" "${HS_LANGUAGE_JSON}" "${HS_LANGUAGE_FILE}" 
+getSyntaxFile "${CSS_REPO}/${CSS_TAG}/${CSS_SYNTAX_DIR}" "${CSS_LANGUAGE_JSON}" ""
