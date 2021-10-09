3771a3b2a95b5c0fc3e09b9b0cf86bbeb4b72006
isao-takejib <isao.takeji.bigmoon@gmail.com>
2021-10-10 01:09:02 +0900

    Add script to generate debug files from tests

---
+++ generateDebugFile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -
+
+set -eux
+
+LANGNAME=${1}
+TESTS="./test/tests"
+TESTFILES="${TESTS}/${LANGNAME}/*.${LANGNAME}"
+DEBUGDIR="./debug/"
+DEBUGFILE="${DEBUGDIR}/debug.${LANGNAME}"
+LINECOMMENT=""
+
+mkdir -p "${DEBUGDIR}"
+
+if [ -f "${DEBUGFILE}" ]; then
+    mv "${DEBUGFILE}" "${DEBUGFILE}.bak"
+fi
+
+case "${LANGNAME}" in
+    hamlet) LINECOMMENT="\$#" ;;
+    cassius | lucius) LINECOMMENT="//" ;;
+    yesodroutes) LINECOMMENT="--" ;;
+    *) echo "There are no test cases."
+        exit 1;;
+esac
+
+for f in ${TESTFILES}; do
+    grep -v "^${LINECOMMENT}" "${f}" >> "${DEBUGFILE}"
+done
